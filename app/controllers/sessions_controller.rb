2d54a124c30833b7d4cc5974b03e5b50f3cea18f
mntj <mntj11@gmail.com>
2014-07-11 13:21:48 -0400

    sessions controller changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,20 @@
 class SessionsController < ApplicationController
 
   def create
-    user = User.from_omniauth(env["omniauth.auth"])
-    session[:user_id] = user.id
+    @user = User.find_or_create_from_auth_hash(auth_hash)
+    self.current_user = @user
+    session[:user_id] = @user.id
     redirect_to root_path
   end
 
   def destroy
     session[:user_id] = nil
     redirect_to root_path
+  end
+
+  protected
+
+  def auth_hash
+    request.env['omniauth.auth']
   end
 end
