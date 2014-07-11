cd0d78648ce4ff2e62d9e98ee711bf7c20f53778
mntj <mntj11@gmail.com>
2014-07-11 15:10:38 -0400

    add username column to users table

---
+++ db/migrate/20140711190921_add_github_username_columnto_users_table.rb
@@ -0,0 +1,5 @@
+class AddGithubUsernameColumntoUsersTable < ActiveRecord::Migration
+  def change
+    add_column :users, :github_username, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140711140205) do
+ActiveRecord::Schema.define(version: 20140711190921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20140711140205) do
     t.string   "email",              null: false
     t.string   "crypted_password",   null: false
     t.string   "salt",               null: false
+    t.string   "github_username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
