f29eecd1f671bf9d6da1460eaccd2e1ddd180fb4
mntj <mntj11@gmail.com>
2014-07-12 12:03:18 -0400

    give user a random email if they do not have one in their GitHub info

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,20 @@
 class User < ActiveRecord::Base
+  require 'digest/sha1'
   authenticates_with_sorcery!
   def self.from_omniauth(auth)
     where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
-      binding.pry
       user.provider = auth.provider
       user.uid = auth.uid
       user.name = auth.info.name
       user.github_username = auth.info.nickname
       user.oauth_token = auth.credentials.token
-      #Github tokens never expire!
-      #user.oauth_expires_at = Time.at(auth.credentials.expires_at)
-      user.github_email = auth.info.email
       user.github_profile_img = auth.info.image
-      user.email = user.github_email || "anon@ymous.com"
-      user.password = "notarealpassword"
+      user.repos_url = auth.extra.raw_info.repos_url
+      rand_string = (0...8).map { (65 + rand(26)).chr }.join
+      user.github_email = auth.info.email
+      user.github_email = rand_string + "@example.com" if user.github_email.empty?
+      user.email = user.github_email
+      user.password = Digest::SHA1.hexdigest rand_string
       user.save!
     end
   end
